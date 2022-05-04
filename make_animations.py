f30983d9d717d41d965291340efaa6690640a522
sk3619 <sk3619@ic.ac.uk>
2022-05-04 09:12:22 +0100

    no seed shift no erosion

--- make_animations.py
+++ make_animations.py
@@ -7,7 +7,8 @@ import plotoptix.materials as m
 from plotoptix import NpOptiX, TkOptiX
 from scipy import ndimage
 import moviepy.editor as mp
-
+import imageio
+import os
 # Define project name
 parser = argparse.ArgumentParser()
 parser.add_argument('micro', type=str)
@@ -19,7 +20,7 @@ Project_dir = 'Trained_Generators/micros'
 # Run with False to show an image during or after training
 parser = argparse.ArgumentParser()
 Project_path = util.mkdr(Project_name, Project_dir, False)
-
+ 
 image_type = 'grayscale'
 data_type = 'png'
 data_path = ['Examples/micro378.png']
@@ -38,7 +39,7 @@ netG = netG()
 netG.eval()
 lf = 8
 n = (lf-2) * 32
-noise = torch.randn(10, z_channels, lf, lf, lf)
+noise = torch.randn(1, z_channels, lf, lf, lf)
 netG = netG.cuda()
 noise = noise.cuda()
 nseeds=10
@@ -53,41 +54,50 @@ print(img.shape, image_type)
 img = util.post_proc(img, image_type)
 if image_type =='twophase':
     ph = 0 if np.mean(img) > 0.5 else 1
+if image_type == 'twophase':
+    bind = np.array(np.where(img == ph)).T
+    c = 1 -(bind + 0.5)
+    c = (0.3,0.3,0.3)
+else:
+    # img = ndimage.gaussian_filter(img, blur)
+    bind = np.array(np.where(img !=-1)).T
+    # bind[:,1][img.reshape(-1) <  0] +=1000
+    c = (img.reshape(-1))/255
+    print(c.max(), c.min(), c.shape)
+bind = bind/n - 0.5
 
 class params():
     tf = 360
     f = 0
     s = int(360/tf)
-    e = [3, 1.2, 0]
-    l = [0, 10, 5]
-    bind = None
-    c = None
+    e = [-3, 1.2, 0]
+    l = [-5, 10, 0]
+    bind = bind
+    c = c
 def compute(rt: NpOptiX, delta: int) -> None: # compute scene updates in parallel to the raytracing
     interval = int(params.f//interval_size)
     # print(interval, params.f)
-    noise1 = noise[interval]
-    try:
-        noise2 = noise[interval+1]
-    except:
-        noise2 = noise[0]
-    prog = (params.f%interval_size)/interval_size
-    # prog = prog if prog <=1 else 2- prog
-    # print(prog, interval, params.f)
-    noiset = (noise1 * (1-prog)) + (noise2*prog)
-    img = util.post_proc(netG(noiset.unsqueeze(0)), image_type)
-    if image_type == 'twophase':
+    # noise1 = noise[interval]
+    # try:
+    #     noise2 = noise[interval+1]
+    # except:
+    #     noise2 = noise[0]
+    # prog = (params.f%interval_size)/interval_size
 
-        bind = np.array(np.where(img == ph)).T
-        params.c = 1 -(bind + 0.5)
-        params.c = (0.3,0.3,0.3)
-    else:
-        img = ndimage.gaussian_filter(img, blur)
-        bind = np.array(np.where(img !=-1)).T
-        cutoff = params.f/180 if params.f < 180 else 2 - params.f/180
-        bind[:,1][img.reshape(-1) < img.max() * cutoff * 0.5] +=1000
-        params.c = img.reshape(-1)/255
+    # noiset = (noise1 * (1-prog)) + (noise2*prog)
+    # img = util.post_proc(netG(noise), image_type)
+    # if image_type == 'twophase':
+    #     # bind = np.array(np.where(img == ph)).T
+    #     # params.c = 1 -(bind + 0.5)
+    #     params.c = (0.3,0.3,0.3)
+    # else:
+    #    img = ndimage.gaussian_filter(img, blur)
+    #    bind = np.array(np.where(img !=-1)).T
+        # cutoff = params.f/180 if params.f < 180 else 2 - params.f/180
+        # bind[:,1][img.reshape(-1) < img.max() * cutoff * 0.5] +=1000
+        # params.c = img.reshape(-1)/100
 
-    params.bind = bind/n - 0.5
+    # params.bind = bind/n - 0.5
     params.f+=params.s
     f_step = params.f * np.pi * 2 / 360
     # params.e = [0.5*np.cos(params.f/360), 12, 20*np.sin(params.f/360)]
@@ -123,75 +133,44 @@ def animate(netG):
                     width=width, height=height,
                     start_now=False)
 
-    # optix = TkOptiX(start_now=False) # no need to open the window yet
     optix.set_param(min_accumulation_step=min_accum,    # 1 animation frame = 128 accumulation frames
                     max_accumulation_frames=5130,
                     light_shading="Hard")  # accumulate 512 frames when paused
     optix.set_uint("path_seg_range", 5, 10)
-    exposure = 0.8; gamma = 2.2
+    exposure = 1; gamma = 2.3
     optix.set_float("tonemap_exposure", exposure) # sRGB tonning
     optix.set_float("tonemap_gamma", gamma)
     optix.set_float("denoiser_blend", 0.25)
     optix.add_postproc("Denoiser")
-    # optix.set_ambient([0.1, 0.2, 0.3])
     optix.set_background(255)
+    # img = util.post_proc(netG(noise[0].unsqueeze(0)), image_type)
+    # if image_type == 'twophase':
 
-
-
-    # optix.set_float("tonemap_exposure", 0.5)
-    # optix.set_float("tonemap_gamma", 2.2)
-
-    # optix.add_postproc("Gamma") 
-
-    img = util.post_proc(netG(noise[0].unsqueeze(0)), image_type)
-    # print(img.shape)
-    if image_type == 'twophase':
-
-        bind = np.array(np.where(img == ph)).T
-        c = 1 -(bind + 0.5)
-        c = (0.3,0.3,0.3)
-    else:
-        img = ndimage.gaussian_filter(img, blur)
-        bind = np.array(np.where(img !=-1)).T
-        bind[:,1][img.reshape(-1) <  0] +=1000
-        c = (img.reshape(-1))/100
+    #     bind = np.array(np.where(img == ph)).T
+    #     c = 1 -(bind + 0.5)
+    #     c = (0.3,0.3,0.3)
+    # else:
+    #     # img = ndimage.gaussian_filter(img, blur)
+    #     bind = np.array(np.where(img !=-1)).T
+    #     # bind[:,1][img.reshape(-1) <  0] +=1000
+    #     c = (img.reshape(-1))/100
     n = img.shape[0]
     s=1/n
-    bind = bind/n - 0.5
-    # use "Hard" light shading for the best caustics and "Soft" for fast convergence
-
-    # optix.set_uint("path_seg_range", 15, 30)
-
     alpha = np.full((1, 1, 4), 1, dtype=np.float32)
     optix.set_texture_2d("mask", (255*alpha).astype(np.uint8))
     m_diffuse_3 = make_material("Diffuse", color_tex="mask")
-    # m_specular_1 = make_material("Reflective", specular=0.9)
-    # m_glass_4 = make_material("Transmissive", refraction_index=[1.35, 1.4, 1.48])
-    # m_metal_1 = make_material("Reflective", metalness=1.0, roughness=0.004)
     optix.setup_material("3", m_diffuse_3)
-    # optix.setup_material("1", m_specular_1)
-    # optix.setup_material("3", m_metal_1)
-
     optix.set_data("cubes_b", pos=bind, u=[s, 0, 0], v=[0, s, 0], w=[0, 0, s],
                 geom="Parallelepipeds", # cubes, actually default geometry
                 mat="3",          # opaque, mat, default
                 c = c)
     optix.setup_camera("cam1", eye=params.e, target=[0,0,0], up=[0,1, 0], fov=30)
-    # optix.set_background(100)
     optix.set_ambient((0.3, 0.3, 0.3))
-
-
-    optix.set_float("tonemap_exposure", 0.5)
-    optix.set_float("tonemap_gamma", 2.2)
-
-    optix.add_postproc("Gamma")      # apply gamma correction postprocessing stage, or
     x = n/2
-    # optix.setup_light("light1", pos=params.l, color=2*np.array([1.0, 1.0, 1.0]), radius=2)
     optix.start()
 
 def save_animation():
-    import imageio
-    import os
+    
     images = []
     frames = sorted(os.listdir('frames'))
     for filename in frames[1:]:
