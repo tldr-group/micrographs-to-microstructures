b086ef0fe0121e2e4d50ab0f063a55120370444e
amirDahari1 <amir.dahari@mail.huji.ac.il>
2022-05-04 19:57:29 +0300

    Data augmentation of random flip/rotation

--- make_animations.py
+++ make_animations.py
@@ -1,6 +1,7 @@
 from slicegan import networks, util
 import argparse
 import torch
+import tifffile
 import numpy as np
 from plotoptix.materials import make_material
 import plotoptix.materials as m
@@ -12,75 +13,87 @@ from moviepy.editor import *
 import imageio
 import os
 from time import time
+
 start = time()
 # Define project name
 parser = argparse.ArgumentParser()
 parser.add_argument('micro', type=str)
 args = parser.parse_args()
 
-Project_name = f'micro{args.micro}'
+Project_name = f'microstructure{args.micro}'
 # Specify project folder.
-Project_dir = 'Trained_Generators/micros'
+Project_dir = 'Trained_Generators'
 # Run with False to show an image during or after training
 parser = argparse.ArgumentParser()
 Project_path = util.mkdr(Project_name, Project_dir, False)
- 
+
+os.makedirs(Project_path + 'frames', exist_ok=True)
+
 image_type = 'grayscale'
 data_type = 'png'
 data_path = ['Examples/micro378.png']
 
-img_size, img_channels, scale_factor = 64, 1,  1
+img_size, img_channels, scale_factor = 64, 1, 1
 z_channels = 16
 lays = 6
-dk, gk = [4]*lays, [4]*lays
-ds, gs = [2]*lays, [2]*lays
-df, gf = [img_channels,64,128,256,512,1], [z_channels,512,256,128,64,img_channels]
-dp, gp = [1,1,1,1,0],[2,2,2,2,3]
+dk, gk = [4] * lays, [4] * lays
+ds, gs = [2] * lays, [2] * lays
+df, gf = [img_channels, 64, 128, 256, 512, 1], [z_channels, 512, 256, 128, 64,
+                                                img_channels]
+dp, gp = [1, 1, 1, 1, 0], [2, 2, 2, 2, 3]
 
 ## Create Networks
-netD, netG = networks.slicegan_nets(Project_path, False, image_type, dk, ds, df,dp, gk ,gs, gf, gp)
+netD, netG = networks.slicegan_nets(Project_path, False, image_type, dk, ds,
+                                    df, dp, gk, gs, gf, gp)
 netG = netG()
 netG.eval()
 lf = 8
-n = (lf-2) * 32
+n = (lf - 2) * 32
 noise = torch.randn(1, z_channels, lf, lf, lf)
 netG = netG.cuda()
 noise = noise.cuda()
-nseeds=10
+nseeds = 10
 blur = 0.75
-interval_size = 360/nseeds
+interval_size = 360 / nseeds
 netG.load_state_dict(torch.load(Project_path + '_Gen.pt'))
 res = 512
 min_accum = 500
 img = netG(noise[0].unsqueeze(0))
-image_type = 'twophase' if img.shape[1]==2 else 'grayscale' 
+image_type = 'twophase' if img.shape[1] == 2 else 'grayscale'
 print(img.shape, image_type)
 img = util.post_proc(img, image_type)
-if image_type =='twophase':
+tif = np.int_(img)
+tifffile.imsave(Project_path + ".tif", tif)
+
+if image_type == 'twophase':
     ph = 0 if np.mean(img) > 0.5 else 1
 if image_type == 'twophase':
     bind = np.array(np.where(img == ph)).T
-    c = 1 -(bind + 0.5)
-    c = (0.3,0.3,0.3)
+    c = 1 - (bind + 0.5)
+    c = (0.3, 0.3, 0.3)
 else:
     # img = ndimage.gaussian_filter(img, blur)
-    bind = np.array(np.where(img !=-1)).T
+    bind = np.array(np.where(img != -1)).T
     # bind[:,1][img.reshape(-1) <  0] +=1000
-    c = (img.reshape(-1))/255
+    c = (img.reshape(-1)) / 255
     print(c.max(), c.min(), c.shape)
-bind = bind/n - 0.5
+bind = bind / n - 0.5
+
 
 class params():
     tf = 360
     f = 0
-    s = int(360/tf)
+    s = int(360 / tf)
     e = [-3, 1.2, 0]
     l = [-5, 10, 0]
     bind = bind
     c = c
     fin = False
-def compute(rt: NpOptiX, delta: int) -> None: # compute scene updates in parallel to the raytracing
-    interval = int(params.f//interval_size)
+
+
+def compute(rt: NpOptiX,
+            delta: int) -> None:  # compute scene updates in parallel to the raytracing
+    interval = int(params.f // interval_size)
     # print(interval, params.f)
     # noise1 = noise[interval]
     # try:
@@ -98,54 +111,59 @@ def compute(rt: NpOptiX, delta: int) -> None: # compute scene updates in paralle
     # else:
     #    img = ndimage.gaussian_filter(img, blur)
     #    bind = np.array(np.where(img !=-1)).T
-        # cutoff = params.f/180 if params.f < 180 else 2 - params.f/180
-        # bind[:,1][img.reshape(-1) < img.max() * cutoff * 0.5] +=1000
-        # params.c = img.reshape(-1)/100
+    # cutoff = params.f/180 if params.f < 180 else 2 - params.f/180
+    # bind[:,1][img.reshape(-1) < img.max() * cutoff * 0.5] +=1000
+    # params.c = img.reshape(-1)/100
 
     # params.bind = bind/n - 0.5
-    params.f+=params.s
+    params.f += params.s
     f_step = params.f * np.pi * 2 / 360
     # params.e = [0.5*np.cos(params.f/360), 12, 20*np.sin(params.f/360)]
     x, y = np.cos(f_step), np.sin(f_step)
-    
-    params.e = [-3*x, 1.2, -3*y]
-    params.l = [-5*x, 5, -5*y]
+
+    params.e = [-3 * x, 1.2, -3 * y]
+    params.l = [-5 * x, 5, -5 * y]
     # print(params.e)
-    
+
+
 # optionally, save every frame to a separate file using save_image() method
-def update(rt: NpOptiX) -> None: 
+def update(rt: NpOptiX) -> None:
     rt.update_camera('cam1', eye=params.e)
     # rt.update_light('light1', pos=params.l)
 
-    rt.set_data("cubes_b", pos=params.bind, u=[1/n, 0, 0], v=[0, 1/n, 0], w=[0, 0, 1/n],
-                geom="Parallelepipeds", # cubes, actually default geometry
-                mat="diffuse",          # opaque, mat, default
-                c = params.c)
+    rt.set_data("cubes_b", pos=params.bind, u=[1 / n, 0, 0], v=[0, 1 / n, 0],
+                w=[0, 0, 1 / n],
+                geom="Parallelepipeds",  # cubes, actually default geometry
+                mat="diffuse",  # opaque, mat, default
+                c=params.c)
     # rt.update_light('light1', pos=params.l)
     # print("frames/frame_{:05d}.png".format(params.f))
-    rt.save_image("frames/frame_{:05d}.png".format(params.f))    
-    if params.f%nseeds ==0:
+    rt.save_image(Project_path + "frames/frame_{:05d}.png".format(params.f))
+    if params.f % nseeds == 0:
         print(params.f, time() - start)
 
-    if params.f==360:
+    if params.f == 360:
         save_animation()
         rt.close()
         params.fin = True
-        
+
 
 def animate(netG):
-    width = res; height = res
+    width = res;
+    height = res
     optix = NpOptiX(on_scene_compute=compute,
                     on_rt_completed=update,
                     width=width, height=height,
                     start_now=False)
 
-    optix.set_param(min_accumulation_step=min_accum,    # 1 animation frame = 128 accumulation frames
+    optix.set_param(min_accumulation_step=min_accum,
+                    # 1 animation frame = 128 accumulation frames
                     max_accumulation_frames=5130,
                     light_shading="Hard")  # accumulate 512 frames when paused
     optix.set_uint("path_seg_range", 5, 10)
-    exposure = 1; gamma = 2.3
-    optix.set_float("tonemap_exposure", exposure) # sRGB tonning
+    exposure = 1;
+    gamma = 2.3
+    optix.set_float("tonemap_exposure", exposure)  # sRGB tonning
     optix.set_float("tonemap_gamma", gamma)
     optix.set_float("denoiser_blend", 0.25)
     optix.add_postproc("Denoiser")
@@ -162,31 +180,33 @@ def animate(netG):
     #     # bind[:,1][img.reshape(-1) <  0] +=1000
     #     c = (img.reshape(-1))/100
     n = img.shape[0]
-    s=1/n
+    s = 1 / n
     alpha = np.full((1, 1, 4), 1, dtype=np.float32)
-    optix.set_texture_2d("mask", (255*alpha).astype(np.uint8))
+    optix.set_texture_2d("mask", (255 * alpha).astype(np.uint8))
     m_diffuse_3 = make_material("Diffuse", color_tex="mask")
     optix.setup_material("3", m_diffuse_3)
     optix.set_data("cubes_b", pos=bind, u=[s, 0, 0], v=[0, s, 0], w=[0, 0, s],
-                geom="Parallelepipeds", # cubes, actually default geometry
-                mat="3",          # opaque, mat, default
-                c = c)
-    optix.setup_camera("cam1", eye=params.e, target=[0,0,0], up=[0,1, 0], fov=30)
+                   geom="Parallelepipeds",  # cubes, actually default geometry
+                   mat="3",  # opaque, mat, default
+                   c=c)
+    optix.setup_camera("cam1", eye=params.e, target=[0, 0, 0], up=[0, 1, 0],
+                       fov=30)
     optix.set_ambient((0.3, 0.3, 0.3))
-    x = n/2
+    x = n / 2
     optix.start()
-    while not params.fin:
-        pass
+
+
 def save_animation():
-    frames = sorted(os.listdir('frames'))
+    frames = sorted(os.listdir(Project_path + 'frames'))
     print('loading frames')
     fps = 45
-    frame_duration = 1/fps
-    clips = [ImageClip(f'frames/{m}').set_duration(frame_duration)
+    frame_duration = 1 / fps
+    clips = [
+        ImageClip(f'{Project_path}frames/{m}').set_duration(frame_duration)
         for m in frames]
     clip = concatenate_videoclips(clips, method="compose")
-    clip.write_videofile(f'animations/{Project_name}.mp4', fps=fps)
-    
+    clip.write_videofile(f'{Project_path}.mp4', fps=fps)
+
 
 animate(netG)
 # save_animation()
--- web/info_scraper.py
+++ web/info_scraper.py
@@ -1,8 +1,20 @@
 import requests  # to get image from the web
 from bs4 import BeautifulSoup
 
+text_to_key = {
+    "name": "Micrograph 1",
+    "keyword": "Keywords",
+    "category": "Categories",
+    "brief_description": "Brief description",
+    "long_description": "Further information",
+    "element": "Composition",
+    "technique": "Technique",
+    "data_type": "grayscale",
+    "contributor": "Contributor",
+    "organisation": "Organisation"
+  }
 
-def find_and_print_text(soup_block, header):
+def find_and_print_text(soup_block, header, return_list=False):
     """
     :param soup_block: The beautifulsoup object
     :param header: The header for the text to be returned
@@ -23,6 +35,27 @@ for i in range(1, 21):
     soup_info = soup_info.findAll()
     print(f'Micrograph number: {i}')
     for block in soup_info:
+        keys = ['Keywords', 'Brief description', 'Further information', ]
+        find_and_print_text(block, 'Keywords')
         find_and_print_text(block, 'Brief description')
         find_and_print_text(block, 'Further information')
     print()
+
+
+# "0": {
+#     "name": "Micrograph 1",
+#     "keyword": ["alloy", "nickel"],
+#     "category": ["ceramic", "fracture"],
+#     "brief_description": "dnfiluahwrfilsuhf",
+#     "long_description": "kfjghlsieuhrgliusehrliguhe",
+#     "element": ["Cu", "O"],
+#     "technique": "SEM",
+#     "data_type": "grayscale",
+#     "contributor": "Michael",
+#     "organisation": "University",
+#     "link_doitpoms": "www.hello.com",
+#     "data_2D": "snapshot2.png",
+#     "data_3D": "snapshot2.png",
+#     "preview": "snapshot2.png",
+#     "movie": "snapshot2.png"
+#   }
